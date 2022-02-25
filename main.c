c8157af4c7eacd71046a3c142f12355c954c4bac
Rodkin Ilya <rid843@mail.ru>
2022-02-25 22:20:09 +0700

    Add geometry(Pars,S,P)

--- main.c
+++ main.c
@@ -1,6 +1,47 @@
-#include "stdio.h"
+#include <stdio.h>
+#include <string.h>
+double PI = 3.141592;
+int error_code(char str[]);
+double pars_strok(char str[]);
 
-int main() {
-  printf("Hello, world!");
-  return 0;
+int main()
+{
+    char str[100];
+    double n = 0, perimeter = 0, area = 0;
+    gets(str);
+    if (error_code(str) == 1) {
+        n = pars_strok(str);
+        perimeter = 2 * PI * n;
+        area = PI * pow(n, 2);
+        printf("P = %.4f\n", perimeter);
+        printf("S = %.4f", area);
+    }
+}
+
+int error_code(char str[])
+{
+    char* figure = strstr(str, "circle");
+    if (figure == NULL) {
+        printf("Error at column 0: expected 'circle'\n\n");
+    } else {
+        return 1;
+    }
+}
+
+double pars_strok(char str[])
+{
+    double x = 0, y = 0, rad;
+    char *probel_x, *probel_y, *probel_radius;
+    for (int i = 0; i < 100; i++) {
+        if (str[i - 1] == '(')
+            probel_x = &str[i];
+        if (str[i - 1] == ',')
+            probel_radius = &str[i];
+        if (str[i + 1] == '\0')
+            break;
+    }
+    x = strtod(probel_x, &probel_y);
+    y = strtod(probel_y, NULL);
+    rad = strtod(probel_radius, NULL);
+    return rad;
 }
